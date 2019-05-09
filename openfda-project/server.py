e1fd1b6e7c5a5bfcfd29748f9a950ebde61c18ff
carolbertoncini <46898679+carolbertoncini@users.noreply.github.com>
2019-05-09 08:58:36 +0200

    Update server.py

--- openfda-project/server.py
+++ openfda-project/server.py
@@ -1,3 +1,4 @@
+#Importamos los módulos necesarios
 import http.server
 import socketserver
 import json
@@ -5,39 +6,39 @@ import http.client
 
 IP = "localhost"
 PORT = 8000
-socketserver.TCPServer.allow_reuse_address = True
+socketserver.TCPServer.allow_reuse_address = True #Para no tener que ccambair el puerto
 
 URL = "api.fda.gov"
-
-class OpenFDA_HTML():
+#Extensioń III: creamos un HTML para poder visualizar la información
+class OpenFDA_HTML(): 
     def html(self, list1):
         inicio = "<!doctype html>" + "\n" + "<html>" + "\n" + "<body>" + "\n" "<ul>" + "\n"
         final = "</ul>" + "\n" + "</body>" + "\n" + "</html>"
 
-        with open("drug.html", "w") as f:
+        with open("drug.html", "w") as f:   #Nuevo .text que creamos con los datos
             f.write(inicio)
             for elemento in list1:
-                elemento1 = "<li>" + elemento + "</li>" + "\n"
+                elemento1 = "<li>" + elemento + "</li>" + "\n" #Iteramos sobre los elementos de la lsita
                 f.write(elemento1)
             f.write(final)
 
 
 HTML = OpenFDA_HTML()
 
-
+#Extensión III: nos comunicamos con la API de FDA
 class OpenFDA_Client():
     def buscar_medicamento(self, medicamento, limit):
         headers = {"User-Agent": "http-client"}
         conexion = http.client.HTTPSConnection(URL)
-        url_buscar_medicamento = "/drug/label.json?search=active_ingredient:" + medicamento + "&" + "limit=" + limit
-        conexion.request("GET", url_buscar_medicamento, None, headers)
-        respuesta1 = conexion.getresponse()
-        drugs_raw = respuesta1.read().decode("utf-8")
+        url_buscar_medicamento = "/drug/label.json?search=active_ingredient:" + medicamento + "&" + "limit=" + limit #URL para busacr el medicamento
+        conexion.request("GET", url_buscar_medicamento, None, headers) #GET: enviamos solicitud
+        respuesta1 = conexion.getresponse() #Obtenemos la respuesta de la solicitud enviada
+        drugs_raw = respuesta1.read().decode("utf-8") #Convertimos la información para que sea legible
         conexion.close()
-        medicamento = json.loads(drugs_raw)
+        medicamento = json.loads(drugs_raw) #Convertimos la información a JSON
         medicamento1 = medicamento
         return medicamento1
-
+#Volvemos a hacer lo mismo para buscar al fabricante y para buscar listas enteras.
     def buscar_fabricante(self, medicamento, limit):
         headers = {"User-Agent": "http-client"}
         conexion = http.client.HTTPSConnection(URL)
@@ -65,7 +66,7 @@ class OpenFDA_Client():
 
 Client = OpenFDA_Client()
 
-
+#Extensión III: obtenemos los datos de los medicamentos.
 class OpenFDA_Parser():
     def datos_medicamentos(self, medicamento_1, list1):
         for i in range(len(medicamento_1["results"])):
@@ -97,7 +98,7 @@ class OpenFDA_Parser():
                     list1.append(fabricante_1["results"][i]["openfda"]["manufacturer_name"][0])
             except KeyError:
                 list1.append("Unknown")
-
+#Extensón I: obetenrmos un listado ocn las advertencias de los mediamentos
     def warnings(self, warning_1, list1):
         for i in range(len(warning_1["results"])):
             if "warnings" in warning_1["results"][i]:
@@ -114,20 +115,20 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         try:
 
-            if self.path == '/':
-                self.send_response(200)
-                self.send_header("Content-type", "text/html")
+            if self.path == '/': #Construimos la página de inicio a través de esta condición (que el recurso sea /).
+                self.send_response(200) #Primera línea del mensaje de respuesta en el status, indicamos que OK.
+                self.send_header("Content-type", "text/html") #Vamos colocando en las siguientes líenas de resouesta las cabeceras necesarias para que el cliente entienda el contenido que le mandamos (HTML).
                 self.end_headers()
                 with open("search.html", "r") as f:
                     data = f.read()
-                    self.wfile.write(bytes(data, "utf8"))
+                    self.wfile.write(bytes(data, "utf8")) #Escribimos la pagina en HTML.
 
-            elif "searchDrug" in self.path:
+            elif "searchDrug" in self.path: #Si el recurso solicitado es Buscar un medicamento
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
                 self.end_headers()
 
-                list1 = []
+                list1 = [] #Creamos una lista para almacenar todos los medicamentos
 
                 if "&" not in self.path:
                     limit = "10"
@@ -140,21 +141,21 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 elif "&" in self.path:
                     parametro = self.path.split("?")[1]
                     medicamento = parametro.split("&")[0].split("=")[1]
-                    limit = parametro.split("&")[1].split("=")[1]
+                    limit = parametro.split("&")[1].split("=")[1] #Extensión I: introducimos limites.
 
                     if not limit:
-                        limit = "10"
+                        limit = "10" #Si no se introduce un limite establezco 10 por defecto.
 
                     buscar = Client.buscar_medicamento(medicamento, limit)
                     Parser.datos_medicamentos(buscar, list1)
 
-                HTML.html(list1)
+                HTML.html(list1)  
 
-                with open("drug.html", "r") as f:
+                with open("drug.html", "r") as f:  
                     file = f.read()
 
-                self.wfile.write(bytes(file, "utf8"))
-
+                self.wfile.write(bytes(file, "utf8")) #Se escriben los medicamentos en HTML.
+            #Vamos estableciendo lo mismo para cada uno de los recursos solicitados.
             elif "searchCompany" in self.path:
                 self.send_response(200)
                 self.send_header("Content-type", "text/html")
@@ -262,7 +263,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
                 with open("error.html", "r") as f:
                     file = f.read()
                 self.wfile.write(bytes(file, "utf8"))
-
+#EXTENSIÓN II: implementamos error 404.
         except KeyError:
             self.send_response(404)
             self.send_header("Content-type", "text/html")
@@ -273,7 +274,7 @@ class testHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
 
         return
 
-
+#El servidor comienza aquí
 Handler = testHTTPRequestHandler
 
 httpd = socketserver.TCPServer((IP, PORT), Handler)
